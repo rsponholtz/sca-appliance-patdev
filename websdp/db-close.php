b34a66b12f5b6393d23a96da29c2726d177683e4
Jason Record <g23guy@gmail.com>
2014-05-07 21:30:12 -0600

    ready for mysqli

--- websdp/db-close.php
+++
@@ -1,5 +0,0 @@
-<?PHP //echo "<!-- Modified: Date       = 2014 Jan 22 -->\n"; ?>
-<?PHP
-	mysql_close($DBConn) or die("<FONT SIZE=\"-1\"><B>ERROR</B>: Unable to close database: $Database</FONT><BR>");
-	//echo "<!-- Database: Status          = Closed -->\n\n";
-?>
--- websdp/db-open.php
+++
@@ -1,7 +0,0 @@
-<?PHP //echo "<!-- Modified: Date       = 2014 Jan 22 -->\n"; ?>
-<?PHP
-	//echo "\n<!-- Database: Connecting      = $Database on $DBHost as $User -->\n";
-	$DBConn = mysql_connect($DBHost,$User,$Password) or die("<FONT SIZE=\"-1\"><B>ERROR</B>: Unable to connect to database: <b>$Database</b></FONT><BR>");
-	@mysql_select_db($Database) or die("<FONT SIZE=\"-1\"><B>ERROR</B>: Unable to select database: <b>$Database</b></FONT><BR>");
-	//echo "<!-- Database: Status          = Open -->\n\n";
-?>
